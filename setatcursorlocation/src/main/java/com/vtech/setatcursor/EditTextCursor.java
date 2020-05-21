fbf42c7030e856df4f9b6cc476dacbf171084fc9
vksevaliya <vijendrasevaliya@gmail.com>
2020-05-21 10:49:09 +0530

    removing text at a cursor location added

--- setatcursorlocation/src/main/java/com/vtech/setatcursor/EditTextCursor.java
+++ setatcursorlocation/src/main/java/com/vtech/setatcursor/EditTextCursor.java
@@ -33,4 +33,48 @@ public class EditTextCursor {
     }
 
 
+    /**
+     * @param editText provide EditText where you want to remove text at cursor location
+     */
+
+    @SuppressLint("SetTextI18n")
+    public static void removeText(EditText editText) {
+        if (editText.getText().length() != 0) {
+            if (editText.getText().length() == 1) editText.setText("");
+            else {
+
+                int length = editText.length();
+                int pos = editText.getSelectionStart();
+                if (pos == length) {
+                    editText.setText(editText.getText().subSequence(0, editText.length() - 1));
+                    editText.setSelection(pos - 1);
+                } else {
+                    String s = editText.getText().toString();
+                    String one = s.substring(0, pos);
+                    String two = s.substring(pos, length);
+                    int newpos = pos;
+                    if (one.length() != 0) {
+                        if (one.length() == 1) {
+                            one = "";
+                            newpos = 0;
+                        } else {
+                            one = one.substring(0, pos - 1);
+                            newpos--;
+                        }
+                    } else {
+                        newpos = 0;
+                    }
+                    editText.setText(one + two);
+                    editText.setSelection(newpos);
+
+                }
+
+
+            }
+        }
+
+
+    }
+
+
 }
